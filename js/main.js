c63e283bc3f446ff6985591598b92165146fb7df
Mike <mhmalone@hawkmail.hfcc.edu>
2019-12-16 17:43:56 -0500

    updated birth date

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 12, 2019").getTime();
+var countDownDate = new Date("Dec 12, 2020).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
